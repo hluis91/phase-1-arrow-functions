5e781916bade8cd680d944f0a210e0c890fe0c50
Luis Hernandez <lshrnndz6@gmail.com>
2022-12-21 10:54:50 -0800

    commit tree c07a5d812120627686c3b472beac99d15f0d3824

---
+++ index.js
@@ -0,0 +1,58 @@
+const divide = function(a,b){
+    return a/b
+}
+
+const square = x => x**2;
+
+const add = (a,b) => a + b;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Arrow function for divison
+// const divide = (a, b) => a/b;
+
+// // Arrow function for square
+// const square = x => x**2;
+
+// //Arrow function for add
+// const add = (a, b) => a + b;
